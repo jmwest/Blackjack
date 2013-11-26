a68040076cdda8617a127aacf64a7b0ed4711ea0
JohnWest <johnwest@umich.edu>
2013-11-26 11:19:46 -0500

    Fixed Player_test11

--- Player_test11.cpp
+++ Player_test11.cpp
@@ -52,7 +52,7 @@ int main()
 		}
 		else if (i < 8)
 		{
-			player->expose(card_array[i]);
+			player->expose(card_array[1]);
 			player->expose(card_array[i]);
 
 			assert(player->bet(100, 25) == 25);
@@ -60,16 +60,48 @@ int main()
 			assert(player->bet(100, 5) == 5);
 			assert(player->bet(100, 2) == 2);
 			assert(player->bet(100, 0) == 0);
+
+			player->expose(card_array[2]);
+
+			assert(player->bet(100, 25) == 50);
+			assert(player->bet(100, 10) == 20);
+			assert(player->bet(100, 5) == 10);
+			assert(player->bet(100, 2) == 4);
+			assert(player->bet(100, 0) == 0);
+
+			player->expose(card_array[i]);
+
+			assert(player->bet(100, 25) == 50);
+			assert(player->bet(100, 10) == 20);
+			assert(player->bet(100, 5) == 10);
+			assert(player->bet(100, 2) == 4);
+			assert(player->bet(100, 0) == 0);
 		}
 		else
 		{
-			player->expose(card_array[i]);
+			player->expose(card_array[1]);
+			player->expose(card_array[2]);
+
+			assert(player->bet(100, 25) == 50);
+			assert(player->bet(100, 10) == 20);
+			assert(player->bet(100, 5) == 10);
+			assert(player->bet(100, 2) == 4);
+			assert(player->bet(100, 0) == 0);
+
 			player->expose(card_array[i]);
 
 			assert(player->bet(100, 25) == 25);
 			assert(player->bet(100, 10) == 10);
 			assert(player->bet(100, 5) == 5);
 			assert(player->bet(100, 2) == 2);
+			assert(player->bet(100, 0) == 0);
+
+			player->expose(card_array[3]);
+			
+			assert(player->bet(100, 25) == 50);
+			assert(player->bet(100, 10) == 20);
+			assert(player->bet(100, 5) == 10);
+			assert(player->bet(100, 2) == 4);
 			assert(player->bet(100, 0) == 0);
 		}
 	}
